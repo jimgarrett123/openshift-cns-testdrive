30cff2b73675407b478360fa45e35a6284065ce4
jgarrett <jimgarrett123@gmail.com>
2020-10-01 13:58:06 -0400

    new files

---
+++ workshop-settings.sh
@@ -0,0 +1,10 @@
+API_URL=https://api.jag.jagocp-rhdemo.com:6443
+MASTER_URL=https://console-openshift-console.apps.jag.jagocp-rhdemo.com
+KUBEADMIN_PASSWORD=IGvRJ-7IJcA-LoqPk-W7KBK
+SSH_USERNAME=user1
+SSH_PASSWORD=openshift
+BASTION_FQDN=foo.bar.com
+GUID=XXX
+ROUTE_SUBDOMAIN=apps.jag.jagocp-rhdemo.com
+HOME_PATH=/opt/app-root/src
+
