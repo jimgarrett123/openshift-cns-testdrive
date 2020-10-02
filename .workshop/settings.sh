17187a7fa5257adff4f25390a6ba5b7ac0b10e8e
jgarrett <jimgarrett123@gmail.com>
2020-10-02 09:02:33 -0400

    new files

---
+++ .workshop/settings.sh
@@ -0,0 +1,7 @@
+WORKSHOP_NAME=ocp-ocs-admins
+WORKSHOP_IMAGE=quay.io/jgarrett0/openshift-cns-testdrive:master
+WORKSHOP_TITLE="OpenShift and Container Storage for Admins"
+RESOURCE_BUDGET=medium
+MAX_SESSION_AGE=3600
+IDLE_TIMEOUT=300
+SPAWNER_MODE=terminal-server
